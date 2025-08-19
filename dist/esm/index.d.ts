5550650f0acadb84cf489c9a5a151cfd6bd86346
Adhi <adhiprst@gmail.com>
2025-08-19 11:08:00 +0700

    remove /dist

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { MockLocationCheckerPlugin } from './definitions';
-declare const MockLocationChecker: MockLocationCheckerPlugin;
-export * from './definitions';
-export { MockLocationChecker };
